9508df63d4382d55b6291c5fd6bd0c899cd04ca9
web-luisaguilar <web.luisaguilar@gmail.com>
2022-12-08 20:07:05 -0600

    first run server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import express from 'express'
+//import express from 'express'
+const express = require('express')
 
 const app = express()
+const port = process.env.port || 3000
 
-app.listen()
+app.get('/', (req, res) => {
+  res.send('<h1> helo world!</h1>')
+})
+app.listen(port, () =>
+  console.log(`Server is ready up on: https://localhost:${port}`)
+)
